5abdce42b5f69176cb080a565fcc0f68bd2a3356
CodeBlaze <blazeingamer10@gmail.com>
2020-10-04 13:52:22 +0530

    swipe octal direction detection done

---
+++ Assets/Scripts/Input/SwipeInputDetector.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+
+using UnityEngine;
+
+namespace CodeBlaze.Detris.Input {
+
+    public class SwipeInputDetector : MonoBehaviour {
+
+        [SerializeField] private float _swipeThreshold = 10f;
+
+        public static event EventHandler<SwipeEventArgs> Swipe;
+
+        public bool Active { get; set; } = true;
+        
+        private Vector2 _fingerDown;
+        private Vector2 _fingerUp;
+
+        private bool _swiping;
+
+        private void Update() {
+            if (!Active) return;
+
+            foreach (var touch in UnityEngine.Input.touches) {
+                switch (touch.phase) {
+                    case TouchPhase.Began:
+                        _fingerDown = touch.position;
+
+                        break;
+                    case TouchPhase.Moved:
+                        _fingerUp = touch.position;
+                        CheckSwipe();
+
+                        break;
+                    case TouchPhase.Ended:
+                        _swiping = false;
+
+                        break;
+                }
+            }
+        }
+
+        private void CheckSwipe() {
+            if (_swiping) return;
+
+            var swipe = _fingerUp - _fingerDown;
+
+            if (!(swipe.magnitude >= _swipeThreshold)) return;
+
+            _swiping = true;
+
+            Swipe?.Invoke(this, new SwipeEventArgs {
+                StartPosition = _fingerDown,
+                EndPosition = _fingerUp
+            });
+        }
+
+        public class SwipeEventArgs : EventArgs {
+            
+            public Vector2 StartPosition { get; set; }
+            public Vector2 EndPosition { get; set; }
+
+        }
+
+    }
+    
+    public enum SwipeDirection : byte {
+
+        NORTH,
+        NORTH_EAST,
+        EAST,
+        SOUTH_EAST,
+        SOUTH,
+        SOUTH_WEST,
+        WEST,
+        NORTH_WEST
+
+    }
+
+    public static class SwipeHelpers {
+
+        public static SwipeDirection GetOctalDirection(SwipeInputDetector.SwipeEventArgs e) {
+            var angle = Vector2.SignedAngle(Vector2.right, e.EndPosition - e.StartPosition);
+
+            return GetOctalDirection(angle);
+        }
+
+        public static SwipeDirection GetOctalDirection(float angle) {
+            if (angle < 22.5f && angle >= -22.5f) return SwipeDirection.EAST;
+            
+            if (angle < 67.5f && angle >= 22.5f) return SwipeDirection.NORTH_EAST;
+            if (angle < 112.5f && angle >= 67.5f) return SwipeDirection.NORTH;
+            if (angle < 157.5f && angle >= 112.5f) return SwipeDirection.NORTH_WEST;
+
+            if (angle < -22.5f && angle >= -67.5f) return SwipeDirection.SOUTH_EAST;
+            if (angle < -67.5f && angle >= -112.5f) return SwipeDirection.SOUTH;
+            if (angle < -112.5f && angle >= -157.5f) return SwipeDirection.SOUTH_WEST;
+
+            if ((angle < -157.5f && angle >= -180f) || (angle <= 180f && angle >= 157.5f)) return SwipeDirection.WEST;
+            
+            throw new ArgumentException($"Invalid swipe angle : {angle}");
+        }
+
+        public static SwipeDirection GetHorizontalDirection(SwipeInputDetector.SwipeEventArgs e) {
+            var angle = Vector2.SignedAngle(Vector2.right, e.EndPosition - e.StartPosition);
+
+            return GetHorizontalDirection(angle);
+        }
+
+        public static SwipeDirection GetHorizontalDirection(float angle) {
+            if (angle < 90f && angle >= -90f) return SwipeDirection.EAST;
+            if ((angle >= 90f && angle <= 180f) || (angle <= -90f && angle >= -180f)) return SwipeDirection.WEST;
+            
+            throw new ArgumentException($"Invalid swipe angle : {angle}");
+        }
+
+        public static float MeanX(SwipeInputDetector.SwipeEventArgs e) {
+            return (e.EndPosition.x + e.StartPosition.x) / 2;
+        }
+        
+        public static float MeanY(SwipeInputDetector.SwipeEventArgs e) {
+            return (e.EndPosition.y + e.StartPosition.y) / 2;
+        }
+        
+    }
+
+}
--- Assets/Scripts/SceneRootController.cs
+++ Assets/Scripts/SceneRootController.cs
@@ -11,30 +11,43 @@ namespace CodeBlaze.Detris {
     public class SceneRootController : MonoBehaviour {
 
         [SerializeField] [Range(0.5f, 5f)] private float _rotationDuration = 1f;
-
+        [SerializeField] [Range(0.2f, 0.5f)] private float _screenYSplit = 0.3f;
+        
         private Vector3 _rotation;
 
         private void Start() {
             _rotation = Vector3.zero;
-            TouchInputManager.SwipeRight += SwipeRight;
-            TouchInputManager.SwipeLeft += SwipeLeft;
+            SwipeInputDetector.Swipe += OnSwipe;
         }
 
         private void OnDestroy() {
-            TouchInputManager.SwipeRight -= SwipeRight;
-            TouchInputManager.SwipeLeft -= SwipeLeft;
+            SwipeInputDetector.Swipe -= OnSwipe;
         }
 
-        private void SwipeLeft(object sender, TouchInputManager.SwipeEventArgs e) {
-            _rotation += Vector3.up * -90;
-            if (Math.Abs(_rotation.y + 360) < float.Epsilon) _rotation = Vector3.zero;
-            transform.DORotate(_rotation, _rotationDuration);
+        private void OnSwipe(object sender, SwipeInputDetector.SwipeEventArgs e) {
+            if (SwipeHelpers.MeanY(e) / Screen.height > _screenYSplit) ShapeMovement(e);
+            else SceneRotation(e);
         }
 
-        private void SwipeRight(object sender, TouchInputManager.SwipeEventArgs e) {
-            _rotation += Vector3.up * 90;
-            if (Math.Abs(_rotation.y - 360) < float.Epsilon) _rotation = Vector3.zero;
-            transform.DORotate(_rotation, _rotationDuration);
+        private void ShapeMovement(SwipeInputDetector.SwipeEventArgs e) {
+            Debug.Log($"Movement Direction : {SwipeHelpers.GetOctalDirection(e)}");
+        }
+
+        private void SceneRotation(SwipeInputDetector.SwipeEventArgs e) {
+            switch (SwipeHelpers.GetHorizontalDirection(e)) {
+                case SwipeDirection.WEST:
+                    _rotation += Vector3.up * 90;
+                    if (Math.Abs(_rotation.y - 360) < float.Epsilon) _rotation = Vector3.zero;
+                    transform.DORotate(_rotation, _rotationDuration);
+
+                    break;
+                case SwipeDirection.EAST:
+                    _rotation += Vector3.up * -90;
+                    if (Math.Abs(_rotation.y + 360) < float.Epsilon) _rotation = Vector3.zero;
+                    transform.DORotate(_rotation, _rotationDuration);
+
+                    break;
+            }
         }
 
     }
